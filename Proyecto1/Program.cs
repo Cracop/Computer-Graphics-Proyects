3db1b459b12c3f7a2234c85be989712b466a138c
cracop <rodrigoalejandrobarrera@gmail.com>
2021-09-29 16:36:24 -0500

    Se copió la teselación de la tarea 2

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,26 @@
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+//Rodrigo Barrera 181935
+namespace Proyecto1
+{
+     public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Rodrigo Barrera - 181935 - Teselaciones Penrose",
+            };
+
+            // To create a new window, create a class that extends GameWindow, then call Run() on it.
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+
+            // And that's it! That's all it takes to create a window with OpenTK.
+        }
+    }
+}
+
---
+++ Proyecto1/Shaders/shaderAmarillo.frag
@@ -0,0 +1,8 @@
+#version 330
+
+out vec4 outputColor;
+
+void main()
+{
+    outputColor = vec4(0.87, 0.734, 0.082, 1.0);
+}
