2bf98de436e7195b4e7612326c4ed9171f0c5a63
cracop <rodrigoalejandrobarrera@gmail.com>
2021-11-01 18:35:46 -0600

    Ya existe la teselación 2D en espacio 3D y me puedo mover

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -20,6 +20,7 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void reflect(unsigned int vao, int toDraw,Shader ourShader, glm::mat4 transform);
 void processInput(GLFWwindow *window);
 
 // settings
@@ -85,84 +86,134 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+  
+    float verticesTes[] = {
+        0.0f,0.2f,0.0f, //D 0
+        0.025f,0.1f,0.0f, //E 1
+        0.0f,0.1f,0.0f, //J 2
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.095f,0.16f,0.0f, //C 3
+        0.07f,0.068f,0.0f, //F 4
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.16f,0.1f,0.0f, //H 5
+        0.1,0.025,0.0f, //G 6
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.2f,0.0f,0.0f, //I 7
+        0.1f,0.0f,0.0f, //K 8
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.07f,0.26f,0.0f, //L 9
+        0.2f,0.2f,0.0f, //M 10
+        0.26f,0.07f,0.0f, //N 11
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        0.0f,0.33f,0.0f, //O 12
+        0.16f,0.29f,0.0f, //P 13
+        0.29f,0.16f,0.0f, //Q 14
+        0.33f,0.0f,0.0f, //R 15
+
+        0.0f,0.0f,0.0f, //Cero 16
     };
-    // world space positions of our cubes
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+
+//vamos de adentro a afuera
+    //centro amarillo
+    unsigned int indicesCentro[] = {
+        2,1,16,
+        1,4,16,
+        4,6,16,
+        6,8,16
     };
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    //triangulos amarillos primer cuadrante
+    unsigned int indicesI[] = {
+        0,1,2,
+        1,3,4,
+        4,5,6,
+        6,7,8
+    };
+    //triangulos rojos
+    unsigned int indicesR[] = {
+         0,3,1,
+         0,9,3,
+         4,3,5,
+         3,10,5,
+         6,5,7,
+         5,11,7
+    };
+    //triangulos azules claros
+    unsigned int indicesA[] = {
+        0,12,9,
+        3,9,13,
+        3,10,13,
+        5,10,14,
+        5,11,14,
+        11,7,15
+    };
 
-    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //Creo los VAOs, VBOs y EBOs
+    // ------------------------------------
+    unsigned int VBOs[10], VAOs[10], EBOs[10];
+    glGenVertexArrays(10, VAOs);
+    glGenBuffers(10, VBOs);
+    glGenBuffers(10, EBOs);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    //Para los CIRCULOS 
+    //==========Primer Circulo==================
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAOs[1]);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTes), verticesTes, GL_STATIC_DRAW);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesI), indicesI, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
 
+    //==========Segundo Circulo pt1==================
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAOs[2]);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTes), verticesTes, GL_STATIC_DRAW);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[2]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesR), indicesR, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    //==========Segundo Circulo pt2==================
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAOs[3]);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[3]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTes), verticesTes, GL_STATIC_DRAW);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[3]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesA), indicesA, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    //==========Centro==================
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAOs[4]);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[4]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTes), verticesTes, GL_STATIC_DRAW);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[4]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesCentro), indicesCentro, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 
     ourShader.use();
-    ourShader.setVec4("ourColor", 0.07, 0.27f, 0.37f, 1.0f);
-
-
-
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -182,8 +233,6 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
-
-
         // activate shader
         ourShader.use();
 
@@ -195,20 +244,40 @@ int main()
         glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("view", view);
 
-        // render boxes
-        glBindVertexArray(VAO);
-        for (unsigned int i = 0; i < 10; i++)
-        {
-            // calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i; 
-            angle = glfwGetTime() * 25.0f * pow(-1,i);
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
-            
-            glDrawArrays(GL_TRIANGLES, 0, 36); 
-        }
+        glm::mat4 transform;
+
+        //==========Primer Circulo==================
+        transform = glm::mat4(1.0f);
+        ourShader.setMat4("transform", transform);
+        ourShader.setVec4("ourColor", 0.87f, 0.734f, 0.082f, 1.0f);
+        glBindVertexArray(VAOs[1]);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+        reflect(VAOs[1], 12, ourShader,transform);
+
+        //========Segundo Circulo pt1===============
+        transform = glm::mat4(1.0f);
+        ourShader.setMat4("transform", transform);
+        ourShader.setVec4("ourColor", 1.0f, 0.0f, 0.0f, 1.0f);
+        glBindVertexArray(VAOs[2]);
+        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
+        reflect(VAOs[2], 18, ourShader, transform);
+        
+        //=========Segundo Circulo pt2==============
+        transform = glm::mat4(1.0f);
+        ourShader.setMat4("transform", transform);
+        ourShader.setVec4("ourColor", 0.0980f, 0.0980f, 0.439f, 1.0f);
+        glBindVertexArray(VAOs[3]);
+        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
+        reflect(VAOs[3], 18, ourShader, transform);
+
+        //================Centro====================
+        transform = glm::mat4(1.0f);
+        ourShader.setMat4("transform", transform);
+        ourShader.setVec4("ourColor", 0.87f, 0.734f, 0.082f, 1.0f);
+        glBindVertexArray(VAOs[4]);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+        reflect(VAOs[4], 12, ourShader,transform);
+        
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -218,8 +287,8 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(10, VAOs);
+    glDeleteBuffers(10, VBOs);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -232,6 +301,9 @@ int main()
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -279,6 +351,26 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
+}
+
+//Refleja a todos los cuadrantes
+void reflect(unsigned int vao, int toDraw,Shader ourShader, glm::mat4 transform) {
+    //glm::mat4 transform = glm::mat4(1.0f);
+    //Transformacion (reflexion x)
+    transform = glm::scale(transform, glm::vec3(1, -1, 1));
+    ourShader.setMat4("transform", transform);
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, toDraw, GL_UNSIGNED_INT, 0);
+    //Transformacion (reflexion y)
+    transform = glm::scale(transform, glm::vec3(-1, 1, 1));
+    ourShader.setMat4("transform", transform);
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, toDraw, GL_UNSIGNED_INT, 0);
+    //Transformacion (reflexion x)
+    transform = glm::scale(transform, glm::vec3(1, -1, 1));
+    ourShader.setMat4("transform", transform);
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, toDraw, GL_UNSIGNED_INT, 0);
 }
 
 //g++ 7.4.camera.cpp glad.c -ldl -lglfw
